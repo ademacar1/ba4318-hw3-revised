f8783cd777a5dff989899eee3c2c60a93b5fa441
ademacar1 <56065316+ademacar1@users.noreply.github.com>
2019-10-16 11:19:49 +0300

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,11 @@
+number = int( input("age"))
+
+
+if 19 < number :
+    print ("adult")
+    
+if 10 < number <= 19 :
+    print (" adolescent (children)")
+    
+if 0 < number <=1 :
+    print (" infant (children)")
